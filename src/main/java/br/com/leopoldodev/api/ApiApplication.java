2bc14bade976f16f168a9ebafbcf4b8c379ab0ad
leo-demetrio <leocdemetrio@gmail.com>
2021-12-14 11:54:57 -0300

    feat: class UserResource and method findById created

--- src/main/java/br/com/leopoldodev/api/ApiApplication.java
+++ src/main/java/br/com/leopoldodev/api/ApiApplication.java
@@ -1,13 +1,18 @@
 package br.com.leopoldodev.api;
 
+import br.com.leopoldodev.api.domain.User;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
+
 	}
 
 }
