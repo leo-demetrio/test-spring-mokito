80ea7b85621a99a2742e2aeeb024edd7a17759c3
leo-demetrio <leocdemetrio@gmail.com>
2021-12-21 11:32:07 -0300

    feat: created method delete

--- src/main/java/br/com/leopoldodev/api/resources/UserResource.java
+++ src/main/java/br/com/leopoldodev/api/resources/UserResource.java
@@ -19,10 +19,11 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class UserResource {
 
+    public static final String ID = "/{id}";
     private ModelMapper mapper;
     private UserService service;
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
@@ -35,13 +36,18 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO) {
         URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(userDTO).getId()).toUri();
+                .fromCurrentRequest().path(ID).buildAndExpand(service.create(userDTO).getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
         userDTO.setId(id);
         return ResponseEntity.ok().body(mapper.map(service.update(userDTO),UserDTO.class));
+    }
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
