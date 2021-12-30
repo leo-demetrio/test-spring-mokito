bb8640894c494e74125a922794cc01f468c619bd
leo-demetrio <leocdemetrio@gmail.com>
2021-12-30 10:21:46 -0300

    test: class UserServiceImpl method delete with success

--- src/test/java/br/com/leopoldodev/api/services/imp/UserServiceImplTest.java
+++ src/test/java/br/com/leopoldodev/api/services/imp/UserServiceImplTest.java
@@ -19,9 +19,7 @@ import java.util.Optional;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
-
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 
 @SpringBootTest
@@ -137,7 +135,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        userServiceImpl.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser() {
