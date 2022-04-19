b183b724014101167643b4c962a2e68ca75f6dec
leo-demetrio <leocdemetrio@gmail.com>
2022-04-19 11:41:27 -0300

    Test handler method objectNotFound

--- src/main/java/br/com/leopoldodev/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/leopoldodev/api/resources/exceptions/ResourceExceptionHandler.java
@@ -14,12 +14,12 @@ import java.time.LocalDateTime;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> dataIntegrationViolationException(ObjectNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request) {
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<StandardError> objectNotFound(DataIntegrityViolationException ex, HttpServletRequest request) {
+    public ResponseEntity<StandardError> dataIntegrationViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
---
+++ src/test/java/br/com/leopoldodev/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,43 @@
+package br.com.leopoldodev.api.resources.exceptions;
+
+import br.com.leopoldodev.api.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResourceExceptionHandlerTest {
+
+    public static final String OBJECT_NOT_FOUND = "Object not found";
+    @InjectMocks
+    private ResourceExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void dataIntegrationViolationException() {
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(new ObjectNotFoundException(OBJECT_NOT_FOUND), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(OBJECT_NOT_FOUND, response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+}
